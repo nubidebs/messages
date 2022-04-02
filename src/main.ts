ac5bde8d10868108a3d79897b2dcc1c9e8da7569
nubidebs <debora.crescenzo@truepill.com>
2022-04-02 20:06:44 +0100

    changed port

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import { MessagesModule } from './messages/messages.module';
 async function bootstrap() {
   const app = await NestFactory.create(MessagesModule);
   app.useGlobalPipes(new ValidationPipe)
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
